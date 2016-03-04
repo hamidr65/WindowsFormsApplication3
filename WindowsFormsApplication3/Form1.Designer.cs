b62756b777068dd42bdd6e3fefaef1469572b782
hamid <hamid@gmail.com>
2016-03-04 19:11:35 +0330

    ok

--- WindowsFormsApplication3/Form1.Designer.cs
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(76, 63);
+            this.textBox1.Location = new System.Drawing.Point(90, 123);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
